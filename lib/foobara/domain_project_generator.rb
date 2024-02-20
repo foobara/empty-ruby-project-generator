8c6d55eb7829860b6593b0a62cac19c73b368f27
Miles Georgi <azimux@gmail.com>
2024-02-20 21:13:35 +0000

    Basic gem boilerplate created by RubyMine

---
+++ lib/foobara/domain_project_generator.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "domain_project_generator/version"
+
+module Foobara
+  module DomainProjectGenerator
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
