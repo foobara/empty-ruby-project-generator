f1d27266282fe34217bedf7f054a53fb8a772ca2
Miles Georgi <azimux@gmail.com>
2026-08-21 07:36:05 -0700

    Make it easier to use different debuggers

--- templates/boot/start.rb
+++ templates/boot/start.rb
@@ -3,8 +3,18 @@ ENV["FOOBARA_ENV"] ||= "development"
 require "bundler/setup"
 
 if ["development", "test"].include?(ENV["FOOBARA_ENV"])
-  require "pry"
-  require "pry-byebug"
+  # :nocov:
+  if ENV["CI"] != "true"
+    if ENV["RUBY_DEBUG"] == "true"
+      require "debug"
+    elsif ENV["USE_PRY"] == "true"
+      require "pry"
+    elsif ENV["USE_PRY_BYEBUG"] == "true"
+      require "pry"
+      require "pry-byebug"
+    end
+  end
+  # :nocov:
 end
 
 require_relative "config"
