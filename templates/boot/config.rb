727af887697a11e91573cd3cb5a6fa7891a20fa0
Miles Georgi <azimux@gmail.com>
2026-08-21 07:36:26 -0700

    Simplify the generated config.rb

--- templates/boot/config.rb
+++ templates/boot/config.rb
@@ -1,9 +1,3 @@
-require "dotenv"
-
 require "foobara/load_dotenv"
 
-Dotenv.require_keys("FOOBARA_ENV")
-
-env = ENV.fetch("FOOBARA_ENV", nil)
-
-Foobara::LoadDotenv.run!(env:)
+Foobara::LoadDotenv.run!(env: ENV.fetch("FOOBARA_ENV"))
