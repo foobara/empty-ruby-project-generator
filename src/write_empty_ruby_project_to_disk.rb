b06001ce870292890fa8ca17e5ef3c58acc6dcbb
zhephyn <zhephynwinter@gmail.com>
2026-09-21 15:50:05 +0300

    fix shell quoting

--- src/write_empty_ruby_project_to_disk.rb
+++ src/write_empty_ruby_project_to_disk.rb
@@ -1,5 +1,6 @@
 require "fileutils"
 require "extract_repo"
+require "shellwords"
 require_relative "generate_empty_ruby_project"
 
 module Foobara
@@ -197,7 +198,7 @@ module Foobara
 
           puts "pushing to github..."
 
-          cmd = "gh repo create --private #{project_config.org_slash_project_kebab} --description #{project_config.description.inspect}"
+          cmd = "gh repo create --private #{project_config.org_slash_project_kebab} --description #{Shellwords.escape(project_config.description)}"
           exit_status = run_cmd_and_write_output(cmd, raise_if_fails: false)
 
           unless exit_status&.success?
